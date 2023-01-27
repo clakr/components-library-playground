a6c153e7b1269abbfde718f17634954f1c31938a
Clark Tolosa <clarktolosa@gmail.com>
2023-01-27 10:01:08 +0800

    added materialui

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -1,5 +1,35 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { Button } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
 function App() {
-  return <div>qweq</div>;
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
